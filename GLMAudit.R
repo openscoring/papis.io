3ac0e4133b4876ca2db4ad78fea3d66a2352d3bd
Villu Ruusmann <villu.ruusmann@gmail.com>
2018-10-13 11:10:38 -0400

    Enabled model verification

--- GLMAudit.R
+++ GLMAudit.R
@@ -4,6 +4,9 @@ audit = read.csv("csv/Audit.csv")
 audit$Adjusted = as.factor(audit$Adjusted)
 
 glm = glm(Adjusted ~ . - Age + cut(Age, breaks = c(0, 18, 65, 100)) + Gender:Education + I(Income / (Hours * 52)), data = audit, family = "binomial")
-#r2pmml::verify(glm, audit[sample(nrow(audit), 20), ])
+
+audit$Adjusted = NULL
+
+glm = r2pmml::verify(glm, audit[sample(nrow(audit), 100), ])
 
 r2pmml::r2pmml(glm, "pmml/GLMAudit.pmml")
